6c35c58b428858fb6b37299e9998044c91708471
abhargkeshari <abhargkeshari@deqode.com>
2023-08-01 15:45:00 +0530

    Learning testcases

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,38 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-export default function Add() {
-  let x = 5, y = 5;
-  return (
-    <>{x + y}</>
-  );
+const Add = (x,y) => {
+  return x + y;
 }
 
-
-export function CounterP() {
-  const [count, setCount] = useState(0);
-
-  function countF() {
-    // setCount(count + 1)
-    setCount((prev)=> {
-    console.log(prev)
-    return prev+1
-  })
-
-  }
-
-  // useEffect(() => {
-  //   document.title = `You clicked ${count} times`;
-  //   localStorage.setItem(count)
-  // });
-
-
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={countF}>
-        Click me
-      </button>
-    </div>
-  );
-}
+export default Add;
---
+++ src/testcases/Add.test.js
@@ -0,0 +1,9 @@
+//Testcases
+
+import Add from "../components/Add";
+
+describe('Give Sum', ()=>{
+    it('1st test',()=>{
+        expect(Add(2,3)).toBe(5);
+    })
+})
