36d6ed3f424f27299e6e1c50d27f05a4e9611c5e
= <abhargkeshari@deqode.com>
2024-05-29 17:01:45 +0530

    Cart reducer logics

--- src/fullStack/learningApp/frontend/src/reducers/cartReducer.js
+++ src/fullStack/learningApp/frontend/src/reducers/cartReducer.js
@@ -1,8 +1,15 @@
 export const cartReducer = (state, action) => {
     switch (action.type) {
         case "ADD_PRODUCTS":
-        return { ...state, products: action.payload }
-    default:
-        break;
+            return { ...state, products: action.payload }
+        case "ADD_TO_CART":
+            return { ...state, cart: [...state.cart, {...action.payload}] }
+        case "REMOVE_FROM_CART":
+            return { ...state, cart: state.cart.filter((item) => {
+                console.log('item.id',item.id,'action id', action.payload.product_id)
+                item.id !== action.payload.product_id
+            })}
+        default:
+            break;
     }
 }
