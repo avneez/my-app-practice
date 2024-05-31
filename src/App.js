241fac21dcd2a9b7cfe252a3af4341e745ff21f1
= <abhargkeshari@deqode.com>
2024-05-31 15:26:59 +0530

    pagination css

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Playercard from './components/PlayerScoreCard/Playercard.jsx';
 import SoloLevelingV1 from './components/SoloLevelingGame/SoloLevelingV1/SoloLevelingV1.jsx';
 import SoloLevelingV2 from './components/SoloLevelingGame/SoloLevelingV2/SoloLevelingV2.jsx';
 import FolderStructureDesign from './components/FolderStructureDesign/FolderStructureDesign.jsx';
+import Pagination from './components/pagination/Pagination.jsx';
 
 function App() {
   const obj={age: '35', d:'se'}
@@ -56,7 +57,8 @@ function App() {
     <Playercard/> */}
     {/* <SoloLevelingV1/> */}
     {/* <SoloLevelingV2/> */}
-    <FolderStructureDesign/>
+    {/* <FolderStructureDesign/> */}
+    <Pagination/>
     </div>
   );
 }
