46f5762e9550653a8d8f002c362c248309ec9e78
avneez <dreamcatcheravneez@gmail.com>
2026-04-07 23:44:49 +0530

    Updated LRU cache methods

--- src/DSA/LRUCache.js
+++ src/DSA/LRUCache.js
@@ -12,14 +12,17 @@ class LRU {
             this.cache.delete(key); //remove the item from the cache
             this.cache.set(key, value); //freshly add the item to the cache
         }
+        console.log("Accessed item:", `{${key}: ${value}}`);
         return value;
     }
 
     setItem(key, value){
-        if(this.cache.has(key)){ // if key is already in cache, delete it
+        console.log("Adding item:", `{${key}: ${value}}`);
+        if(this.cache.has(key)){ // if key is already in cache, delete its
             this.cache.delete(key);
         }
         else if (this.cache.size === this.capacity) {
+            console.log("Capacity full, Removing the least recently used item:", this.first());
             this.cache.delete(this.first()); // remove the first item if capacity is full
         }
         this.cache.set(key, value);
@@ -28,8 +31,20 @@ class LRU {
     first(){
         return this.cache.keys().next().value; // return first key
     }
+
+    // Implement LRU/MRU retrieval methods
+    getLeastRecent() {
+        return Array.from(this.cache)[0];
+    }
+
+    getMostRecent() {
+        return Array.from(this.cache)[this.cache.size - 1];
+    }
+
 }
 
+// e.g. 1
+console.log("Example 1:")
 const LRUCache = new LRU(3);
 LRUCache.setItem("name", "Avneez")
 LRUCache.setItem("age", "24")
@@ -39,3 +54,5 @@ LRUCache.getItem("name")
 console.log(LRUCache.cache)
 LRUCache.setItem("role", "SDE")
 console.log(LRUCache.cache)
+console.log("Least Recent:", LRUCache.getLeastRecent());
+console.log("Most Recent:", LRUCache.getMostRecent());
